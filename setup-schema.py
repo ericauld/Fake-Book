908872f07ae00a156125bbdeef23bc1f88117297
ericauld <ericauld@ucla.edu>
2020-03-01 19:52:14 -0800

    started refactoring process chords, commenting out

--- setup-schema.py
+++ setup-schema.py
@@ -168,25 +168,23 @@ def main():
     VALUES
         ('', '0,4,3'),
         ('m', '0,3,4');
+
+    
     """
 
     # INSERT INTO ChordNoteDegrees()
 
     # INSERT INTO Modes()
 
- #   try:
-    conn = psycopg2.connect(login_info)
-    conn.set_isolation_level(0)
-    cur = conn.cursor()
-    cur.execute(SQL)
-#    except:
-#    print("I am unable to connect to the database")
+    try:
+        conn = psycopg2.connect(login_info)
+        conn.set_isolation_level(0)
+        cur = conn.cursor()
+        cur.execute(SQL)
+    except:
+        print("I am unable to connect to the database")
 
-    # INSERT INTO ChordNoteDegrees()
 
-    # INSERT INTO Modes()
-
-# Make function to input a mode into the 
 def mode_input(mode_name: str, mode_notes: list) -> None:
     pass
     # '''
