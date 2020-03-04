af15fc42915c4303356e9ebec2aef900e6de3443
Eric Auld <eric.auld@gmail.com>
2020-03-03 17:22:56 -0800

    Started update distance table file.

---
+++ update-distance-table.py
@@ -0,0 +1,18 @@
+import psycopg2
+from pathlib import Path
+
+def main():
+    project_folder = Path("/home/ubuntu/Songbook")
+    _key_file = project_folder / "key-file.txt"
+
+    password = input_list[0]
+    login_info = "dbname='postgres' user='postgres' host='localhost' password='{}'".format(password)
+
+    try:
+        conn = psycopg2.connect(login_info)
+        conn.set_isolation_level(0)
+    except:
+        print("I am unable to connect to the database")
+
+if __name__=='__main__':
+    main()
