7f8c7680c89499c33bc089e3bf2807ea66a3fc78
Ubuntu <ubuntu@ip-172-31-28-46.us-east-2.compute.internal>
2020-03-08 01:24:47 +0000

    simple-dash-script-5.py is working with checkbox inputs.

--- simple-dash-script-5.py
+++ simple-dash-script-5.py
@@ -2,6 +2,24 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
+import psycopg2
+from pathlib import Path
+
+project_folder = Path("/home/ubuntu/Songbook")
+_key_file = project_folder / "key-file.txt"
+
+with _key_file.open() as key_file:
+    input_list = key_file.readlines()
+
+password = input_list[0]
+login_info = "dbname='postgres' user='postgres' host='localhost' password='{}'".format(password)
+
+try:
+    conn = psycopg2.connect(login_info)
+    conn.set_isolation_level(0)
+    print("Database connection successful")
+except:
+    print("I am unable to connect to the database")
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -9,16 +27,31 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div([
     dcc.Input(id='my-id', value='initial value', type='text'),
+
+    html.Label('Checkboxes'),
+    dcc.Checklist(
+        id='checkbox_input',
+        options=[
+            {'label': 'New York City', 'value': 'NYC'},
+            {'label': u'Montréal', 'value': 'MTL'},
+            {'label': 'San Francisco', 'value': 'SF'}
+        ],
+        value=['MTL', 'SF']
+    ),
+
     html.Div(id='my-div')
 ])
 
 
 @app.callback(
     Output(component_id='my-div', component_property='children'),
-    [Input(component_id='my-id', component_property='value')]
+    [
+        Input('checkbox_input', 'value'),
+        Input(component_id='my-id', component_property='value')
+    ]
 )
-def update_output_div(input_value):
-    print("assdfd")
+def update_output_div(checkbox_input, input_value):
+    print(checkbox_input)
     return 'You\'ve entered "{}"'.format(input_value)
 
 
